36ffe6dee3a437aaccd93628f4e1fa0f92b0b464
Vladimir Ivakhno <wvovaw0@gmail.com>
2024-05-18 22:39:49 +0500

    feat: add loading spinner

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as UIButton } from "./button";
 export { default as UIInput } from "./input";
 export { default as UIPagination } from "./pagination";
+export { default as UILoading } from "./loading";
