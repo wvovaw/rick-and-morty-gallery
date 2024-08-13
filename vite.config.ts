7528e6b39d6566696cf12c28e4fb07597fe86812
Vladimir Ivakhno <wvovaw0@gmail.com>
2024-08-14 03:11:46 +0500

    make base url an env variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 /// <reference types="vitest" />
 import { resolve } from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-  base: "https://rick-n-morty-gallery.netlify.app/",
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
     },
-  },
-  test: {
-    environment: "jsdom",
-  },
+    test: {
+      environment: "jsdom",
+      coverage: {
+        provider: "v8",
+        exclude: ["node_modules/", "dist/"],
+        all: true,
+      },
+    },
+  };
 });
